e24a5b806119c373e25e7c9c40406de60a0ff1b6
BudCheungChun <98071869+BudCheungChun@users.noreply.github.com>
2024-03-21 12:35:29 +0800

    Update and rename HD_Sem2_人際project.ps1 to HD_Sem2_人際project

--- HD_Sem2_人際project.ps1
+++
@@ -1,3 +0,0 @@
-
-
-powershell -command "$user_profile_path = [Environment]::GetFolderPath('UserProfile');$downloadPath = $user_profile_path + '\Downloads\';Expand-Archive -Path ($downloadPath + 'project_08.zip') -DestinationPath $downloadPath;Move-Item -Path ($downloadPath + 'test_03.ps1'), ($downloadPath + 'SearchEngine_Module_test.py') -Destination $downloadPath;Set-ExecutionPolicy Bypass -Scope Process -Force; & ($downloadPath + 'test_03.ps1')"
